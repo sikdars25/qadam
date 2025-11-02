70e9ca63306669472b47fcf5230a7422b0e6b554
rsikdar25 <rhitwik.sikdar@tcs.com>
2025-11-02 17:53:09 +0530

    fix-all-axios-calls-to-use-jwt-instance

--- frontend/src/components/ChapterQuestions.js
+++ frontend/src/components/ChapterQuestions.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axiosInstance from '../config/axios';  // Use axios instance with JWT
 import './ChapterQuestions.css';
 import API_URL from '../config/api';
 
